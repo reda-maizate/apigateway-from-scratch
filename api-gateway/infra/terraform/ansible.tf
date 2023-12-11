9bc06c473d2dce8f93fb41a7bf0b6e4196d7e650
Réda Maizate <reda.maizate@affluences.com>
2023-12-11 15:47:29 +0100

    infra: finished setting up basic terraform and ansible infra

--- api-gateway/infra/terraform/ansible.tf
+++ api-gateway/infra/terraform/ansible.tf
@@ -1,7 +1,11 @@
 resource "null_resource" "ansible" {
   depends_on = [scaleway_instance_server.my-instance]
 
+  triggers = {
+    build_number = "${timestamp()}"
+  }
+
   provisioner "local-exec" {
-    command = "ansible-playbook -u root -i '${scaleway_instance_server.my-instance.public_ip},' --private-key ${var.scaleway_private_key_path} ../ansible/playbook.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${scaleway_instance_server.my-instance.public_ip},' --private-key ${var.scaleway_private_key_path} ../ansible/playbook.yml"
   }
 }
